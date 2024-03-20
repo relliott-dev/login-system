0350c659535ca3345126b4f9fe3093667504061e
relliott-dev <relliottcreative@gmail.com>
2024-03-20 11:26:37 -0500

    Create footer.php
    Added footer PHP script

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+
+    /*
+        Author: Russell Elliott
+        Date Created: 2024/03/20
+        This file is used to include a consistent footer across all pages
+        
+    */
+    
+?>
+
+<footer class="bg-success text-white text-center p-3 fixed-bottom">
+    <p>&copy; <?php echo date("Y"); ?> Your Website Name. All Rights Reserved.</p>
+</footer>
