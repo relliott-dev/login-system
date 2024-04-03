dacf13f8ba388a472d67654cd93b47aaac191c0b
relliott-dev <relliottcreative@gmail.com>
2024-04-03 11:49:04 -0500

    Update getip.php
    Edited formatting

--- getip.php
+++ getip.php
@@ -1,28 +1,28 @@
 <?php
+	
+	/*
+	* Author: Russell Elliott
+	* Date Created: 2024/03/20
+	*
+	* Retrieves the real IP address of the client
+	* Prioritizes HTTP_CLIENT_IP, then HTTP_X_FORWARDED_FOR, and finally REMOTE_ADDR
+	* 
+	*/
+	
+	$real_ip_address = "";
 
-    /*
-    * Author: Russell Elliott
-    * Date Created: 2024/03/20
-    *
-    * Retrieves the real IP address of the client
-    * Prioritizes HTTP_CLIENT_IP, then HTTP_X_FORWARDED_FOR, and finally REMOTE_ADDR
-    * 
-    */
-
-    $real_ip_address = "";
-
-    if (isset($_SERVER['HTTP_CLIENT_IP']))
-    {
-        $real_ip_address = $_SERVER['HTTP_CLIENT_IP'];
-    }
-    elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-    {
-        $ips = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-        $real_ip_address = trim($ips[0]);
-    }
-    elseif (isset($_SERVER['REMOTE_ADDR']))
-    {
-        $real_ip_address = $_SERVER['REMOTE_ADDR'];
-    }
+	if (isset($_SERVER['HTTP_CLIENT_IP']))
+	{
+		$real_ip_address = $_SERVER['HTTP_CLIENT_IP'];
+	}
+	elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+	{
+		$ips = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+		$real_ip_address = trim($ips[0]);
+	}
+	elseif (isset($_SERVER['REMOTE_ADDR']))
+	{
+		$real_ip_address = $_SERVER['REMOTE_ADDR'];
+	}
 
 ?>
