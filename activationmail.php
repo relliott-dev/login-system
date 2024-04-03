61ecef7f6f5768c1d9d4d6e8d727dee039ab0f13
relliott-dev <relliottcreative@gmail.com>
2024-04-03 10:41:16 -0500

    Create activationmail.php
    Added script to send code in email using PHPMailer

---
+++ activationmail.php
@@ -0,0 +1,55 @@
+<?php
+
+    /*
+    * 
+    * Author: Russell Elliott
+    * Date Created: 2024/03/20
+    * 
+    * This PHP script is used for sending a code activation to a user via email using phpMailer
+    * It uses HTML and sets the recipient, subject and the code
+    * 
+    */
+ 
+    require __DIR__.'/PHPMailer/src/Exception.php';
+    require __DIR__.'/PHPMailer/src/PHPMailer.php';
+    require __DIR__.'/PHPMailer/src/SMTP.php';
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP;
+
+    function smtpmailer($email, $subj, $code)
+    {
+        $mail = new PHPMailer(true);
+        
+        try
+        {
+            $mail->SMTPDebug = 0;
+            $mail->isSMTP();
+            $mail->SMTPAuth = true;
+            $mail->SMTPSecure = 'ssl';
+            $mail->Host = 'mail.example.com';
+            $mail->Port = 465;
+            $mail->Username = 'noreply@example.com';
+            $mail->Password = 'yourpassword';
+    
+            $htmlContent = file_get_contents(__DIR__.'/passwordemail.html');
+            $htmlContent = str_replace('$code', $code, $htmlContent);
+    
+            $mail->isHTML(true);
+            $mail->setFrom('noreply@example.com', 'Support Team');
+            $mail->addReplyTo('noreply@example.com', 'Support Team');
+            $mail->Subject = $subj;
+            $mail->Body = $htmlContent;
+            $mail->addAddress($email);
+    
+            $mail->send();
+            return true;
+        }
+        catch (Exception $e)
+        {
+            return false;
+        }
+    }
+
+?>
