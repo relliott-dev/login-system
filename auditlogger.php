0201cd7461ac36d384ca262d123cd9e24f23e648
relliott-dev <relliottcreative@gmail.com>
2024-04-04 12:08:07 -0500

    Update auditlogger.php
    Added comments

--- auditlogger.php
+++ auditlogger.php
@@ -1,5 +1,14 @@
 <?php
 
+	/*
+	* Author: Russell Elliott
+	* Date Created: 2024/04/02
+	* 
+	* This function is used to log audit events in the database
+	* It records the user ID, action taken, a description of the action, and the IP address of the user
+	* 
+	*/
+
 	function logAudit($db, $userId, $action, $description, $ip)
 	{
 		$query = "INSERT INTO audit_logs (userid, action, description, ip) VALUES (?, ?, ?, ?)";
