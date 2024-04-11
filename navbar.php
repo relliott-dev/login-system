d049911971836dcaf615d37dd1c0a9795cc110e2
relliott-dev <relliottcreative@gmail.com>
2024-04-11 15:44:12 -0500

    Update navbar.php
    Edited navbar for mobile use

--- navbar.php
+++ navbar.php
@@ -13,40 +13,69 @@
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-top">
-	<div class="nav-inner">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="index.php">
-				<img src="images/home-icon.png" alt="Home" class="icon-small home-icon">Your Website
-			</a>
-            
-			<ul class="nav navbar-nav">
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					Page 1
-					</a>
-					<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-						<a class="dropdown-item" href="#">Page 1-1</a>
-						<a class="dropdown-item" href="#">Page 1-2</a>
-						<a class="dropdown-item" href="#">Page 1-3</a>
-					</div>
-				</li>
-				<li class="nav-item"><a href="#" class="nav-link">Page 2</a></li>
-			</ul>
-            
-			<div class="navbar-spacer"></div>
-            
-			<ul class="nav navbar-nav navbar-right">
-				<?php if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin'): ?>
-					<li class="nav-item"><a href="admin.php" class="nav-link"><span class="glyphicon glyphicon-cog"></span> Admin</a></li>
-				<?php endif; ?>
-				<?php if (isset($_SESSION['username'])): ?>
-					<li class="nav-item"><a href="profile.php" class="nav-link"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
-					<li class="nav-item"><a href="logout.php" class="nav-link"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
-				<?php else: ?>
-					<li class="nav-item"><a href="register.php" class="nav-link"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-					<li class="nav-item"><a href="login.php" class="nav-link"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-				<?php endif; ?>
-			</ul>
-		</div>
+	<button class="navbar-toggler left-toggler" type="button">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<a class="navbar-brand mx-auto" href="index.php">
+		<img src="images/home-icon.png" alt="Home" class="icon-small home-icon">
+	</a>
+
+	<button class="navbar-toggler right-toggler" type="button">
+		<img src="images/user-icon.png" alt="Contact" class="icon-small user-icon">
+	</button>
+	
+	<div class="left-panel panel">            
+		<ul class="nav navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Page 1</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+					<a class="dropdown-item" href="#">Page 1-1</a>
+					<a class="dropdown-item" href="#">Page 1-2</a>
+					<a class="dropdown-item" href="#">Page 1-3</a>
+				</div>
+			</li>
+			<li class="nav-item"><a href="#" class="nav-link">Page 2</a></li>
+		</ul>
 	</div>
+
+	<div class="right-panel panel">
+		<ul class="nav navbar-nav">
+			<?php if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin'): ?>
+				<li class="nav-item"><a href="admin.php" class="nav-link"><span class="glyphicon glyphicon-cog"></span> Admin</a></li>
+			<?php endif; ?>
+			<?php if (isset($_SESSION['username'])): ?>
+				<li class="nav-item"><a href="profile.php" class="nav-link"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
+				<li class="nav-item"><a href="logout.php" class="nav-link"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+			<?php else: ?>
+				<li class="nav-item"><a href="register.php" class="nav-link"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+				<li class="nav-item"><a href="login.php" class="nav-link"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+			<?php endif; ?>
+		</ul>
+	</div
+		
 </nav>
+
+<script>
+
+	document.addEventListener('DOMContentLoaded', function ()
+	{
+		const leftToggler = document.querySelector('.left-toggler');
+		const rightToggler = document.querySelector('.right-toggler');
+		const leftPanel = document.querySelector('.left-panel');
+		const rightPanel = document.querySelector('.right-panel');
+
+		leftToggler.addEventListener('click', function ()
+		{
+			leftPanel.classList.toggle('show-left');
+			rightPanel.classList.remove('show-right');
+		});
+
+		rightToggler.addEventListener('click', function ()
+		{
+			rightPanel.classList.toggle('show-right');
+			leftPanel.classList.remove('show-left');
+		});
+	});
+
+</script>
