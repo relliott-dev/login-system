1afe34a0855030427dfd8aeabd08a96ff98961f4
relliott-dev <relliottcreative@gmail.com>
2024-04-17 15:46:38 -0500

    Update getip.php
    removed a redirect

--- getip.php
+++ getip.php
@@ -9,13 +9,6 @@
 	* 
 	*/
 
-	if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest')
-	{
-		http_response_code(403);
-		header("Location: index.php");
-		exit;
-	}
-
 	$real_ip_address = "";
 
 	if (isset($_SERVER['HTTP_CLIENT_IP']))
